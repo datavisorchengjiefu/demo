214eaccf5c5827aa65b8cfb14f76bcd2863465a9
datavisorchengjiefu <chengjie.fu@datavisor.com>
2022-03-09 15:52:13 +0800

    modify

---
+++ src/main/java/com/fcjexample/demo/test/caffeine/CacheLoaderTestAsync.java
@@ -0,0 +1,62 @@
+/*************************************************************************
+ *
+ * Copyright (c) 2022, DATAVISOR, INC.
+ * All rights reserved.
+ * __________________
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of DataVisor, Inc.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to DataVisor, Inc. and
+ * may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from DataVisor, Inc.
+ */
+
+package com.fcjexample.demo.test.caffeine;
+
+import com.github.benmanes.caffeine.cache.AsyncCacheLoader;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+public class CacheLoaderTestAsync implements AsyncCacheLoader<String, DataObject> {
+
+    @Override public @NonNull CompletableFuture<DataObject> asyncLoad(@NonNull String key,
+            @NonNull Executor executor) {
+        CompletableFuture<DataObject> future = CompletableFuture.supplyAsync(
+                new Supplier<DataObject>() {
+                    @Override public DataObject get() {
+                        return DataObject.get("load Data01 ha for " + key);
+                    }
+                });
+
+        return future;
+    }
+
+    @Override public @NonNull CompletableFuture<Map<String, DataObject>> asyncLoadAll(
+            @NonNull Iterable<? extends String> keys, @NonNull Executor executor) {
+
+        CompletableFuture<Map<String, DataObject>> future = CompletableFuture.supplyAsync(
+                new Supplier<Map<String, DataObject>>() {
+                    @Override public Map<String, DataObject> get() {
+                        Map<String, DataObject> map = new HashMap<>();
+                        for (String key : keys) {
+                            if ("A".equals(key) || "D".equals(key)) {
+                                map.put(key, DataObject.get("loadAll Data02 ha for " + key));
+                            }
+                        }
+                        return map;
+                    }
+                });
+
+        return future;
+    }
+
+}
--- src/main/java/com/fcjexample/demo/test/caffeine/Test.java
+++ src/main/java/com/fcjexample/demo/test/caffeine/Test.java
@@ -17,21 +17,19 @@
 
 package com.fcjexample.demo.test.caffeine;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.CacheLoader;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.fcjexample.demo.entity.TestEntity02;
+import com.github.benmanes.caffeine.cache.*;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
@@ -104,9 +102,29 @@ public class Test {
             logger.info("entry is {}, {}. ", entry.getKey(), entry.getValue().getData());
         }
 
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime roundFloor = now.truncatedTo(ChronoUnit.MINUTES);
-        LocalDateTime roundCeiling = now.truncatedTo(ChronoUnit.MINUTES).plusMinutes(1);
+        // loadAll async
+        System.out.println("===============");
+        AsyncCacheLoader<String, DataObject> cacheLoaderAllAsync = new CacheLoaderTestAsync();
+        AsyncLoadingCache<String, DataObject> loadingCacheAllAsync = Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(1, TimeUnit.MINUTES)
+                .buildAsync(cacheLoaderAllAsync);
+
+        CompletableFuture<DataObject> resultAllFuture = loadingCacheAllAsync.get("A");
+        logger.info("entry is {}. ", resultAllFuture.get().getData());
+        resultAllFuture = loadingCacheAllAsync.get("B");
+        logger.info("entry is {}. ", resultAllFuture.get().getData());
+
+        System.out.println("===============");
+        CompletableFuture<Map<String, DataObject>> dataObjectMapAllFuture = loadingCacheAllAsync
+                .getAll(Arrays.asList("A", "B", "C", "D", "E"));
+
+        for (Map.Entry<String, DataObject> entry : dataObjectMapAllFuture.get().entrySet()) {
+            logger.info("entry is {}, {}. ", entry.getKey(), entry.getValue().getData());
+        }
+
+        //
+        System.out.println("==========");
 
         Long minuteWindow = 60 * 1000L;
         //        Long timestamp = System.currentTimeMillis();
@@ -139,5 +157,53 @@ public class Test {
         System.out.println("date01: " + date01);
         System.out.println("date02: " + date02);
 
+        //
+        System.out.println("===========");
+        Long timestamp03 = 1646634070514L;
+        Long timestamp03_01 = 2147483647L;
+        Long timestamp03_02 = 1646634060L;
+        Long timestamp03Minute = timestamp03 - timestamp03 % minuteWindow;
+        System.out.println("timestamp03Minute: " + timestamp03Minute);
+        Date date03 = new Date(timestamp03);
+        Date date03_01 = new Date(timestamp03Minute);
+        System.out.println("date03: " + date03);
+        System.out.println("date03_01: " + date03_01);
+
+        int timestamp03Truncate = truncateByMinute(timestamp03);
+        System.out.println("timestamp03Truncate: " + timestamp03Truncate);
+
+        Map<String, Integer> stringIntegerMap = new HashMap<>();
+        stringIntegerMap.put("3", 3);
+        modifyMap(new HashMap<>(stringIntegerMap));// won't change
+        //        modifyMap(stringIntegerMap);
+        System.out.println("result is " + stringIntegerMap.get("3"));
+
+        Map<String, TestEntity02> testEntity02Map = new HashMap<>();
+        TestEntity02 testEntity02 = new TestEntity02();
+        testEntity02.setName("name_01");
+        testEntity02.setTimeout(10L);
+        testEntity02Map.put("4", testEntity02);
+        modifyTestEntityMap(new HashMap<>(testEntity02Map));// will change
+        //        modifyTestEntityMap(testEntity02Map);
+
+        System.out.println("result02 is " + testEntity02Map.get("3"));
+        System.out.println("result02 is " + testEntity02Map.get("4").getName());
+        System.out.println("result02 is " + testEntity02Map.get("4").getTimeout());
+
+    }
+
+    private static void modifyMap(Map<String, Integer> stringIntegerMap) {
+        stringIntegerMap.put("3", new Integer(4));
+        //        stringIntegerMap.get("3") = 4;
+    }
+
+    private static void modifyTestEntityMap(Map<String, TestEntity02> testEntity02Map) {
+        TestEntity02 temp = testEntity02Map.get("4");
+        temp.setName("after");
+        temp.setTimeout(11L);
+    }
+
+    public static int truncateByMinute(Long timestamp) {
+        return (int) ((timestamp - timestamp % 60000) / 1000);
     }
 }
